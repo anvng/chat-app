04d74ac925abea73bd864ef482e549a894a25ea4
An Van Nguyen <anvndev@gmail.com>
2024-02-24 16:22:12 +0700

    create project v1

---
+++ bookecom/src/main/java/com/example/bookecom/BookecomApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookecom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookecomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookecomApplication.class, args);
+    }
+
+}
---
+++ bookecom/src/test/java/com/example/bookecom/BookecomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookecom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookecomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
